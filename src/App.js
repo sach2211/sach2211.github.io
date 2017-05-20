5c6af3a76c069b9dbbec0f228d28489c4a14753e
sach11 <sachin.chopra@myntra.com>
2017-05-20 18:05:05 +0530

    version 1 ready

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Resume from './components/Resume';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div>
+            <span className="name"> Sachin Chopra </span>
+            <span  className="name-desc"> &lt;Software Developer, JS enthusiast, Travel freek, Bookworm &gt; </span>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Resume />
       </div>
     );
   }
